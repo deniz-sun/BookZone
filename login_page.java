be7ce233772dcdb0c565c75c0f4957cd4a29b2f0
deniz-sun <70107128+deniz-sun@users.noreply.github.com>
2021-12-07 19:19:02 +0300

    Update login_page.java

--- login_page.java
+++ login_page.java
@@ -1,42 +1,99 @@
 package com.bookzone;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class login_page extends JPanel {
-    JLabel email_label;
+public class login_page extends JFrame implements ActionListener {
+
+    Container container = getContentPane();
+    JLabel userLabel = new JLabel("Email: ");
+    JLabel passwordLabel = new JLabel("Password: ");
+    JTextField userTextField = new JTextField();
+    JPasswordField passwordField = new JPasswordField();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+    JCheckBox showPassword = new JCheckBox("Show Password");
     Font font = new Font("Arial", Font.PLAIN, 30);
-    JLabel password_label;
-    TextField email_entry = new TextField(30);
-    TextField password_entry = new TextField(30);
+
+
     public login_page() {
-        Frame frame = new frame();
-        this.setBackground(Color.white);
-      //  this.setPreferredSize(new Dimension(600,300));
-        email_label = new JLabel("Username: ");
-        password_label = new JLabel("Password: ");
-        email_label.setFont(font);
-        password_label.setFont(font);
+        setTitle("Book Zone Login Page");
+        setVisible(true);
+        setSize(1000,700);
+        getContentPane().setBackground(new Color(171,212,182));
 
-        this.add(email_label);
-        this.add(email_entry);
-        this.add(password_label);
-        this.add(password_entry);
-        this.setMaximumSize(new Dimension(600,300));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        container.setLayout(null);
+        setContents();
+        addComponentsToContainer();
+        addActionEvent();
 
-        frame.add(this, BorderLayout.CENTER);
+    }
 
-     //   this.setVisible(true);
-
-
-
+    public void setContents() {
+        showPassword.setBackground(new Color(171,212,182));
+        userLabel.setFont(font);
+        passwordLabel.setFont(font);
+        userLabel.setBounds(250, 250, 150, 30);
+        passwordLabel.setBounds(250, 320, 150, 30);
+        userTextField.setBounds(500, 250, 200, 30);
+        passwordField.setBounds(500, 320, 200, 30);
+        showPassword.setBounds(500, 350, 150, 30);
+        loginButton.setBounds(250, 400, 100, 30);
+        resetButton.setBounds(600, 400, 100, 30);
 
 
     }
 
-    public static void main(String[] args) {
-      //  new frame();
-        new login_page();
+    public void addComponentsToContainer() {
+        container.add(userLabel);
+        container.add(passwordLabel);
+        container.add(userTextField);
+        container.add(passwordField);
+        container.add(showPassword);
+        container.add(loginButton);
+        container.add(resetButton);
     }
 
+    public void addActionEvent() {
+        loginButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        showPassword.addActionListener(this);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginButton) {
+            String userText;
+            String pwdText;
+            userText = userTextField.getText();
+            pwdText = passwordField.getText();
+            if (userText.equalsIgnoreCase("denizsun") && pwdText.equalsIgnoreCase("12345")) {
+                JOptionPane.showMessageDialog(this, "Login Successful");
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
+            }
+
+        }
+        if (e.getSource() == resetButton) {
+            userTextField.setText("");
+            passwordField.setText("");
+        }
+        if (e.getSource() == showPassword) {
+            if (showPassword.isSelected()) {
+                passwordField.setEchoChar((char) 0);
+            } else {
+                passwordField.setEchoChar('*');
+            }
+
+
+        }
+    }
+ public static void main(String[] args){
+
+         new LoginFrame();
+    }
 }
