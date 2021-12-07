5914d53127cc43a668f5580517f2fc335713b3cc
deniz-sun <70107128+deniz-sun@users.noreply.github.com>
2021-12-07 20:00:52 +0300

    Update login_page.java

--- login_page.java
+++ login_page.java
@@ -1,12 +1,13 @@
-package com.bookzone;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class login_page extends JFrame implements ActionListener {
+public class LoginFrame extends JFrame implements ActionListener {
 
     Container container = getContentPane();
+    JLabel welcome = new JLabel("Welcome to ");
+    JLabel bookzone = new JLabel("BookZone");
     JLabel userLabel = new JLabel("Email: ");
     JLabel passwordLabel = new JLabel("Password: ");
     JTextField userTextField = new JTextField();
@@ -15,17 +16,22 @@ public class login_page extends JFrame implements ActionListener {
     JButton resetButton = new JButton("Reset");
     JCheckBox showPassword = new JCheckBox("Show Password");
     Font font = new Font("Arial", Font.PLAIN, 30);
+    Font bookzoneFont = new Font("Arial", Font.BOLD, 40);
+    JButton registration = new JButton();
+    Color color = new Color(171,212,182);
 
 
-    public login_page() {
+    public LoginFrame() {
         setTitle("Book Zone Login Page");
         setVisible(true);
         setSize(1000,700);
-        getContentPane().setBackground(new Color(171,212,182));
+        getContentPane().setBackground(color);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         container.setLayout(null);
+
+
         setContents();
         addComponentsToContainer();
         addActionEvent();
@@ -33,9 +39,17 @@ public class login_page extends JFrame implements ActionListener {
     }
 
     public void setContents() {
-        showPassword.setBackground(new Color(171,212,182));
+        showPassword.setBackground(color);
+        registration.setText("No account? Click here to register!");
+        registration.setForeground(Color.BLUE);
+        registration.setBorderPainted(false);
+        registration.setBackground(color);
         userLabel.setFont(font);
         passwordLabel.setFont(font);
+        welcome.setFont(font);
+        bookzone.setFont(bookzoneFont);
+        welcome.setBounds(250, 100, 200, 30);
+        bookzone.setBounds(425, 90, 200, 45);
         userLabel.setBounds(250, 250, 150, 30);
         passwordLabel.setBounds(250, 320, 150, 30);
         userTextField.setBounds(500, 250, 200, 30);
@@ -43,11 +57,14 @@ public class login_page extends JFrame implements ActionListener {
         showPassword.setBounds(500, 350, 150, 30);
         loginButton.setBounds(250, 400, 100, 30);
         resetButton.setBounds(600, 400, 100, 30);
+        registration.setBounds(400, 440, 300, 30);
 
 
     }
 
     public void addComponentsToContainer() {
+        container.add(welcome);
+        container.add(bookzone);
         container.add(userLabel);
         container.add(passwordLabel);
         container.add(userTextField);
@@ -55,12 +72,14 @@ public class login_page extends JFrame implements ActionListener {
         container.add(showPassword);
         container.add(loginButton);
         container.add(resetButton);
+        container.add(registration);
     }
 
     public void addActionEvent() {
         loginButton.addActionListener(this);
         resetButton.addActionListener(this);
         showPassword.addActionListener(this);
+        registration.addActionListener(this);
     }
 
 
@@ -91,9 +110,9 @@ public class login_page extends JFrame implements ActionListener {
 
 
         }
+        if (e.getSource() == registration){
+            //open the registration page
+        }
     }
- public static void main(String[] args){
 
-         new LoginFrame();
-    }
 }
