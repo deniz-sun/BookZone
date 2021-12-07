a8d493c45d183c6524cec41fd8dfb403320d830f
deniz-sun <70107128+deniz-sun@users.noreply.github.com>
2021-12-07 17:36:40 +0300

    Update login_page.java

--- login_page.java
+++ login_page.java
@@ -1,4 +1,42 @@
-public class login_page{
-  
-  
-} 
+package com.bookzone;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class login_page extends JPanel {
+    JLabel email_label;
+    Font font = new Font("Arial", Font.PLAIN, 30);
+    JLabel password_label;
+    TextField email_entry = new TextField(30);
+    TextField password_entry = new TextField(30);
+    public login_page() {
+        Frame frame = new frame();
+        this.setBackground(Color.white);
+      //  this.setPreferredSize(new Dimension(600,300));
+        email_label = new JLabel("Username: ");
+        password_label = new JLabel("Password: ");
+        email_label.setFont(font);
+        password_label.setFont(font);
+
+        this.add(email_label);
+        this.add(email_entry);
+        this.add(password_label);
+        this.add(password_entry);
+        this.setMaximumSize(new Dimension(600,300));
+
+        frame.add(this, BorderLayout.CENTER);
+
+     //   this.setVisible(true);
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+      //  new frame();
+        new login_page();
+    }
+
+}
