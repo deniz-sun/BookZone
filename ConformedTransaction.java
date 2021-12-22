5219f255c8d39b0f38c96afd05ee59114bd11b77
AlperJoe <91626308+AlperJoe@users.noreply.github.com>
2021-12-22 12:03:18 +0300

    Update ConformedTransaction.java

--- ConformedTransaction.java
+++ ConformedTransaction.java
@@ -5,125 +5,65 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ConfirmedTransaction extends JFrame implements ActionListener{
+
+public class ApproveWindow extends JFrame implements ActionListener {
 
     //properties
-    JPanel panel;
-    JLabel conformedTransaction,bookName,bookType,bookPrice,relatedCourses;
-    JLabel conditionScore,transactionConfirmed,ratedName;
-    JButton courses,rate;
-    GridBagConstraints gbc;
-    Font font = new Font("Arial",Font.PLAIN,18);
+    Container container = getContentPane();
+    Color color = new Color(171,212,182);
+    Font font = new Font("Arial", Font.PLAIN, 20);
+
+    JLabel approveWindow= new JLabel("APPROVE WINDOW");
+    JLabel confirmMessage = new JLabel(" => Exchange has been done!");
+
+    JButton conformedTransaction = new JButton("Conformed Transaction");
 
     //constructor
-    public ConfirmedTransaction(){
-        //frame
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Confirmed Transaction");
-        setSize(1000,700);
-
-
-        //panel
-        panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        panel.setSize(1000,1000);
-        panel.setBackground(new Color(171,212,182));
-        gbc = new GridBagConstraints();
-        gbc.insets = new Insets(30,60,30,60);
-        panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-        add(panel);
-
-        //label
-        conformedTransaction = new JLabel("CONFIRMED TRANSACTION");
-        conformedTransaction.setFont(new Font("Arial",Font.PLAIN,21));
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 10;
-        gbc.fill = GridBagConstraints.CENTER;
-        panel.add(conformedTransaction,gbc);
-
-        bookName = new JLabel("Name:");
-        bookName.setFont(font);
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.gridwidth = 3;
-        gbc.fill = GridBagConstraints.CENTER;
-        panel.add(bookName,gbc);
-
-        bookType = new JLabel("Type:");
-        bookType.setFont(font);
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.gridwidth = 3;
-        gbc.fill = GridBagConstraints.CENTER;
-        panel.add(bookType,gbc);
-
-        bookPrice = new JLabel("Price:");
-        bookPrice.setFont(font);
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        gbc.gridwidth = 3;
-        gbc.fill = GridBagConstraints.CENTER;
-        panel.add(bookPrice,gbc);
-
-        relatedCourses = new JLabel("Related courses:");
-        relatedCourses.setFont(font);
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        gbc.gridwidth = 3;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(relatedCourses,gbc);
-
-        conditionScore = new JLabel("Condition Score");
-        conditionScore.setFont(font);
-        gbc.gridx = 2;
-        gbc.gridy = 4;
-        gbc.gridwidth = 20;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(conditionScore,gbc);
-
-        transactionConfirmed = new JLabel("Please Rate the Other User");
-        transactionConfirmed.setFont(font);
-        gbc.gridx = 1;
-        gbc.gridy = 6;
-        gbc.gridwidth = 4;
-        gbc.fill = GridBagConstraints.CENTER;
-        panel.add(transactionConfirmed,gbc);
-
-        ratedName = new JLabel("Deniz Sun");
-        ratedName.setFont(font);
-        gbc.gridx = 0;
-        gbc.gridy = 7;
-        gbc.gridwidth = 3;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(ratedName,gbc);
-
-        //button
-        courses = new JButton("CS 102");
-        courses.setPreferredSize(new Dimension(80,60));
-        gbc.gridx = 0;
-        gbc.gridy = 5;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(courses,gbc);
-
-        rate = new JButton("RATE");
-        rate.setPreferredSize(new Dimension(80,60));
-        gbc.gridx = 1;
-        gbc.gridy = 7;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.CENTER;
-        panel.add(rate,gbc);
-
+    public ApproveWindow(){
+        setTitle("Approve Window");
         setVisible(true);
+        setSize(1000, 700);
+        getContentPane().setBackground(color);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        container.setLayout(null);
+
+        setContents();
+        addComponentsToContainer();
     }
 
+    //main
     public static void main(String[] args) {
-   //     new ConfirmedTransaction();
+        new ApproveWindow();
+    }
+
+    //methods
+    public void setContents(){
+        approveWindow.setBounds(400,30,500,50);
+        confirmMessage.setBounds(270,350,500,30);
+
+        conformedTransaction.setBounds(600,550,250,60);
+        conformedTransaction.addActionListener(this);
+
+        approveWindow.setFont(font);
+        confirmMessage.setFont(font);
+
+        conformedTransaction.setBackground(Color.gray.brighter());
+        conformedTransaction.setFont(font);
+    }
+
+    public void addComponentsToContainer(){
+        container.add(approveWindow);
+        container.add(confirmMessage);
+        container.add(conformedTransaction);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == conformedTransaction){
+            this.setVisible(false);
+            new ConformedTransaction();
+        }
     }
 }
