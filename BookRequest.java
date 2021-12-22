670cd3c8ea0d5b13758fdbd93f895025980b4bb1
AlperJoe <91626308+AlperJoe@users.noreply.github.com>
2021-12-22 12:00:39 +0300

    Update BookRequest.java

--- BookRequest.java
+++ BookRequest.java
@@ -7,94 +7,82 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BookRequest extends JFrame implements ActionListener{
+
     //properties
-    JFrame frame;
-    JPanel panel;
-    JLabel bookRequest,name,requestMessage;
-    JButton accept,refuse;
-    GridBagConstraints gbc;
+    Container container = getContentPane();
+    Color color =  new Color(171,212,182);
+    Font font = new Font("Arial", Font.PLAIN, 20);
+
+    JLabel bookRequest = new JLabel("BOOK REQUEST");
+    JLabel name = new JLabel("ALPER BOZKURT");
+    JLabel bookPhoto = new JLabel("*Photo of the book*");
+    JLabel requestMessage = new JLabel("'Calculus' wanted by Alper.");
+
+    JButton accept = new JButton("ACCEPT");
+    JButton refuse = new JButton("REFUSE");
 
     //constructor
     public BookRequest(){
-        //frame
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Book Request");
-        frame.setSize(1000, 700);
+        setTitle("Book Request");
+        setVisible(true);
+        setSize(1000, 700);
+        getContentPane().setBackground(color);
 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        container.setLayout(null);
 
-        //panel
-        panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        panel.setSize(1000,1000);
-        panel.setBackground(new Color(171,212,182));
-        gbc = new GridBagConstraints();
-        gbc.insets = new Insets(30,30,10,30);
-        panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-        frame.add(panel);
-
-        //label
-        bookRequest = new JLabel("BOOK REQUEST");
-        bookRequest.setFont(new Font("Arial",Font.PLAIN,21));
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 10;
-        gbc.fill = GridBagConstraints.CENTER;
-        panel.add(bookRequest,gbc);
-
-        name = new JLabel("ALPER BOZKURT");
-        name.setFont(new Font("Arial",Font.PLAIN,18));
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.gridwidth = 3;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(name,gbc);
-
-        requestMessage = new JLabel("'A TextBook of Higher Mathematics'wanted by Alper. Alper offers 100 point.");
-        requestMessage.setFont(new Font("Arial",Font.PLAIN,17));
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.gridwidth = 3;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(requestMessage,gbc);
-
-        //button
-        accept = new JButton("ACCEPT");
-        accept.setPreferredSize(new Dimension(130,60));
-        accept.setBackground(Color.GREEN);
-        accept.addActionListener(this);
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(accept,gbc);
-
-        refuse = new JButton("REFUSE");
-        refuse.setPreferredSize(new Dimension(130,60));
-        refuse.setBackground(Color.GREEN);
-        refuse.addActionListener(this);
-        gbc.gridx = 2;
-        gbc.gridy = 3;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(refuse,gbc);
-
-        frame.setVisible(true);
+        setContents();
+        addComponentsToContainer();
     }
 
+    //main
     public static void main(String[] args) {
         new BookRequest();
     }
 
+    //methods
+    public void setContents(){
+        bookRequest.setBounds(400,30,500,50);
+        name.setBounds(60,80,300,50);
+        bookPhoto.setBounds(40,100,250,350);
+        requestMessage.setBounds(370,250,500,30);
+
+        accept.setBounds(200,550,200,50);
+        refuse.setBounds(600,550,200,50);
+
+        accept.addActionListener(this);
+        refuse.addActionListener(this);
+
+        bookRequest.setFont(font);
+        name.setFont(font);
+        bookPhoto.setFont(font);
+        requestMessage.setFont(font);
+
+        accept.setBackground(Color.gray.brighter());
+        accept.setFont(font);
+        refuse.setBackground(Color.gray.brighter());
+        refuse.setFont(font);
+    }
+
+    public void addComponentsToContainer(){
+        container.add(bookRequest);
+        container.add(name);
+        container.add(bookPhoto);
+        container.add(requestMessage);
+        container.add(accept);
+        container.add(refuse);
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == accept){
+        if(e.getSource() == accept){
             this.setVisible(false);
             new ApproveWindow();
         }
-        else if (e.getSource() == refuse){
+        else if(e.getSource() == refuse){
             this.setVisible(false);
             new MySellOrder();
         }
     }
 }
+
